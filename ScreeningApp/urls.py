352716a035ca4270900e3062f88878aa472afa2d
RISHABH SIDANA <65805267+dinagon@users.noreply.github.com>
2021-08-20 19:42:28 +0530

    Update urls.py

--- ScreeningApp/urls.py
+++ ScreeningApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('screenings/<screening_uuid>/',views.Screening.as_view(),name='screening'),
 	path('screenings/<screening_uuid>/screening-preview/',views.Screening_Preview.as_view(),name='screening_preview'),
 	path('screenings/<screening_uuid>/result/',views.Result.as_view(),name='result'),
+	path('seeans',views.seeans,name='seeans'),
 
 ]
